e48bf392b3a0deb90be0d93e3311fe3fa543395e
idyWilliams <williamsidorenyin@yahoo.com>
2024-03-31 19:30:03 +0100

    updates and new fixes

--- server.js
+++ server.js
@@ -3,19 +3,36 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
+app.use(express.json())
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-    // Start the Express server
-    app.listen(PORT, () => {
+const dataList = [];
+app.get('/data', (req, res) => {
+  res.status(200).send(dataList);
+})
+
+app.post('/data', (req, res) => {
+  let data = req.body;
+  dataList.push(data);
+  res.status(201).send(data);
+  return;
+})
+
+ app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+
+// Connect to MongoDB
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//     // Start the Express server
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
 
 
